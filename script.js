b1a13c81339eafaa33477bace73bd036a0486cb0
t2kworkforcesolutions-boop <t2kworkforcesolutions@gmail.com>
2026-06-26 18:39:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const sections = document.querySelectorAll("section");
 
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:.2
+});
+
+sections.forEach((section)=>{
+    observer.observe(section);
+});
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > 60){
+        nav.style.background = "#0d0d0d";
+    }else{
+        nav.style.background = "rgba(0,0,0,.55)";
+    }
+});
